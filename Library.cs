1607bb572e66d26472013535a44f42a14f38550a
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-22 14:27:15 +0200

    initialize user and library classes

---
+++ Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    internal class Library
+    {
+        public string Code { get; private set; }
+        public string Title { get;  set; }
+        public int Year { get; set; }   
+        public string Theme { get; set; }
+        public int Section { get; set; }
+        public string Author { get; set; }
+    }
+}
